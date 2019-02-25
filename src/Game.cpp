fd8d9bc902fdb3315c264bc347d7b86a02bab03e
tomek <tom.niedzwiedzki@gmail.com>
2019-02-25 16:21:17 +0100

    Review fixed

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,41 @@
 #include "Game.hpp"
-
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<ostream>
+#include<string>
+#include<stdexcept>
 Game::Game() {}
 
 void Game::addFrames(std::string const & frames) {}
+int Game::convertFrameToPoints(std::string input)
+{
+   // char firstCharacter = input[0];
+  //  if (firstCharacter >= '1' and firstCharacter <= '9')
+     //   return firstCharacter -'0';
+     if (input == "-")
+        return 0;
+    else if (input == "X")
+        return 10;
+    else if (input == "1")
+        return 1;
+    else if (input == "2")
+        return 2;
+    else if (input == "3")
+        return 3;
+    else if (input == "4")
+        return 4;
+    else if (input == "5")
+        return 5;
+    else if (input == "6")
+        return 6;
+    else if (input == "7")
+        return 7;
+    else if (input == "8")
+        return 8;
+    else if (input == "9")
+        return 9;
+
+   else
+      throw  std::logic_error("Invalid frame");
+}
--- tests/GameTests.cpp
+++ tests/GameTests.cpp
@@ -1,13 +1,31 @@
 #include <gtest/gtest.h>
 #include "Game.hpp"
+#include <string>
 
 struct GameTests: public ::testing::Test
 {
+    Game game;
 };
 
 TEST_F(GameTests, AddFrames)
 {
     Game game;
     game.addFrames("Someone:32|4");
+}
+
+
+TEST_F(GameTests, convertFrameToPoints)
+{
+    auto result = game.convertFrameToPoints("X");
+    ASSERT_EQ(game.convertFrameToPoints("X"), 10);
+    ASSERT_EQ(game.convertFrameToPoints("-"), 0);
+    ASSERT_EQ(game.convertFrameToPoints("4"), 4);
+}
+
+TEST_F(GameTests, ConvertingWrongFrameCausesException)
+{
+    ASSERT_ANY_THROW(game.convertFrameToPoints("a"));
+    ASSERT_ANY_THROW(game.convertFrameToPoints("10"));
+    ASSERT_ANY_THROW(game.convertFrameToPoints("-3"));
 }
 
