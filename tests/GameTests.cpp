f6599eea7a1800de37f7545fb7ae92015d9754c1
tomasz0 <46450682+tomasz0@users.noreply.github.com>
2019-03-01 13:31:07 +0100

    Update GameTests.cpp

--- tests/GameTests.cpp
+++ tests/GameTests.cpp
@@ -13,21 +13,18 @@ TEST_F(GameTests, AddFrames)
     game.addFrames("Someone:32|4");
 }
 
-
 TEST_F(GameTests, ConvertingSingleCharToPoints_ProperCases)
 {
     ASSERT_EQ(game.convertSingleCharToPoints('X'), 10);
     ASSERT_EQ(game.convertSingleCharToPoints('-'), 0);
     ASSERT_EQ(game.convertSingleCharToPoints('4'), 4);
 }
-
 TEST_F(GameTests, ConvertingSingleCharToPoints_BadCases)
 {
     ASSERT_ANY_THROW(game.convertSingleCharToPoints('a'));
-
+}
 TEST_F(GameTests, Read)
 {
     Game game;
     game.read("text");
-
 }
