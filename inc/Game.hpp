9c9671d34cb46a5de32537b1232a4266ec9f99bc
tomasz0 <46450682+tomasz0@users.noreply.github.com>
2019-03-09 14:09:17 +0100

    Update Game.hpp

--- inc/Game.hpp
+++ inc/Game.hpp
@@ -7,17 +7,13 @@ class Game
 public:
     Game();
     void addFrames(const std::string & frames);
-<<<<<<< HEAD
     int conversion_to_int(std::string input);
-
-=======
     int convertSingleCharToPoints(char input);
 
     void vectorOfDigits(const std::vector<int>& vec);
     int summingOfPointsWithoutBonus(const std::vector<int>& vec);
+    
     void vectorOfDigits2(const std::vector<int>& vec);
     int summingOfBonusPoints(const std::vector<int> &vec);
     int summingOfBonusPointsOfSpare(const std::vector<int>& vec);
->>>>>>> 6c174b324e9caea54ba8897c3aa13f2e8d183438
-
 };
